65d47735439b0c29c7ae2892b92f2b8afcb26ca4
ZingerMarat <93285999+ZingerMarat@users.noreply.github.com>
2023-03-25 01:35:36 +0300

    Show data on screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,19 +8,46 @@ const API_KEY = "7724a6df9ee2e8013130a348969c9ec4";
 //create component
 class App extends React.Component{
 
+    //save data that getted from api
+    state = {
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        sunrise: undefined,
+        sunset: undefined,
+        error: undefined
+    }
+
     /*method that gets weather data async*/
     gettingWeather = async (e) => {
         e.preventDefault();     //cancels page reload on button click
-        const city_name = e.target.elements.city.value
+        var city_name = e.target.elements.city.value
 
-        const api_url = await 
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}&units=metric`);
-        
+        if(city_name){
+            const api_url = await 
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}&units=metric`);        
+    
+            //get json from api response
+            const data = await api_url.json();
 
-        //get json from api response
-        const data = await api_url.json();
+            var sunrise = data.sys.sunrise;
+            var sunriseDate = new Date(sunrise * 1000); // convert seconds to milliseconds
+            var sunrise_date = sunriseDate.toLocaleTimeString(); // format time string
+            
+            var sunset = data.sys.sunset;
+            var sunsetDate = new Date(sunset * 1000); // convert seconds to milliseconds
+            var sunset_date = sunsetDate.toLocaleTimeString(); // format time string
 
-        console.log(data);
+            this.setState({
+                temp: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                sunrise: sunrise_date,
+                sunset: sunset_date,
+                error: ""
+            });
+        }
+
     }
 
     //show html on the screen
@@ -30,7 +57,14 @@ class App extends React.Component{
             <div>
                 <Info/>
                 <Form weatherMethod = {this.gettingWeather}/>
-                <Weather/>
+                <Weather
+                    temp={this.state.temp}
+                    city={this.state.city}
+                    country={this.state.country}
+                    sunrise={this.state.sunrise}
+                    sunset={this.state.sunset}
+                    error={this.state.error}
+                    />
             </div>
         );        
     }
--- my-app/src/components/weather.js
+++ my-app/src/components/weather.js
@@ -5,7 +5,14 @@ class Weather extends React.Component{
     render(){
         return(
           <div>
-            Component with weather
+            { this.props.city &&
+            <div>
+                <p>City: {this.props.city}, {this.props.country}</p>
+                <p>Temperature: {this.props.temp} &deg;C</p>
+                <p>Sunrise: {this.props.sunrise}</p>
+                <p>Sunset: {this.props.sunset}</p>
+            </div>
+            }     
           </div>  
         );
     }
